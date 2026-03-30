e32880657f32972c7bcdccc510c11bd237d3f9b6
Abdulrahman-JS <wea067438@gmail.com>
2026-03-30 10:45:41 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ns=new Scanner(System.in);
+        System.out.println("Please enter your number:");
+        int n=ns.nextInt();
+        System.out.println("Please enter your age:");
+        int m=ns.nextInt();
+        System.out.println("namber:"+n);
+        System.out.println("age:"+m);
+    }
+}
